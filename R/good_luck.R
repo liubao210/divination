5ac01cf299d5772999a90392608436d6228093db
liubao210 <2250790471@qq.com>
2022-01-12 15:10:41 +0800

    添加五行八卦基础数据

--- R/good_luck.R
+++ R/good_luck.R
@@ -1,4 +1,16 @@
 # 运用先天五行八卦，启示吉凶之法
+bagua = data.frame(
+  index = c(1:8),
+  name = c("乾", "兑", "离" ,"震", "巽", "坎", "艮", "坤"),
+  wuxing_index = c(1, 1, 4, 3, 3, 2, 5, 5),
+  wuxing_name = c("金", "金" , "火", "木", "木", "水", "土", "土")
+)
+
+wuxing = data.frame(
+  wuxing_name = c("金" ,"水" ,"木" ,"火", "土"),
+  wuxing_index = c(1, 2, 3, 4, 5)
+)
+
 good_luck <- function(index_1, index_2) {
 
 
